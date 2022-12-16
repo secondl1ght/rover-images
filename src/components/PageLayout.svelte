0991393a611ab87852e2575f178c4b0d0118a582
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:39:58 -0700

    create page layout component

---
+++ src/components/PageLayout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onDestroy } from 'svelte';
+	import { Header, Footer, Image, LoadingSplash } from '$comp';
+	import { roverImage } from '$lib/store';
+
+	onDestroy(() => ($roverImage = undefined));
+</script>
+
+<main>
+	{#if $roverImage}
+		<Header />
+		<Image />
+		<Footer />
+	{:else}
+		<LoadingSplash />
+	{/if}
+</main>
