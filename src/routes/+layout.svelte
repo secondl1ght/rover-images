61acc5796cdbc26e2bbb37ac75e4fb8a52d9465d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:41:42 -0700

    get rover info in layout file across app and set to store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { RoverAPI } from '$lib/wrapper/rover-api';
+
+	const rover = new RoverAPI();
+
+	onMount(() => rover.info());
 </script>
 
 <slot />
