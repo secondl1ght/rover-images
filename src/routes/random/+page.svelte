cebf68ff89d08d91b1c12464271f4d43f5a61434
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:42:48 -0700

    add random image page

---
+++ src/routes/random/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { RoverAPI } from '$lib/wrapper/rover-api';
+	import { PageLayout } from '$comp';
+	import { roverImageTotal } from '$lib/store';
+
+	const getRandomIntInclusive = (min, max) => {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+	};
+
+	const rover = new RoverAPI();
+
+	$: $roverImageTotal && rover.index(getRandomIntInclusive(0, $roverImageTotal));
+</script>
+
+<PageLayout />
