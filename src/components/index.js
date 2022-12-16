2047929029d8ca95dadf1f8832f366755c89a7c6
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:40:19 -0700

    component exports file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Robot } from './Robot.svelte';
+export { default as Header } from './Header.svelte';
+export { default as Footer } from './Footer.svelte';
+export { default as Image } from './Image.svelte';
+export { default as PageLayout } from './PageLayout.svelte';
+export { default as LoadingSplash } from './LoadingSplash.svelte';
