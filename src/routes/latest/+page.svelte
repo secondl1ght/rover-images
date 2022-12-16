62b2c56914de1b556a18ba42f71bbdb16fda8b76
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:42:43 -0700

    add latest image page

---
+++ src/routes/latest/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { onMount } from 'svelte';
+	import { RoverAPI } from '$lib/wrapper/rover-api';
+	import { PageLayout } from '$comp';
+
+	const rover = new RoverAPI();
+	onMount(() => rover.latest());
+</script>
+
+<PageLayout />
