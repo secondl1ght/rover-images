1e9f4b3cdefd44b19541373d38b1ff02a610d303
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:42:32 -0700

    add dynamic route for image indexes

---
+++ src/routes/[index]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	import { browser } from '$app/environment';
+	import { RoverAPI } from '$lib/wrapper/rover-api';
+	import { PageLayout } from '$comp';
+	import { roverImageTotal } from '$lib/store';
+
+	const validateIndex = () => {
+		if (data.index >= 0 && data.index <= $roverImageTotal) {
+			const rover = new RoverAPI();
+			rover.index(data.index);
+		} else {
+			browser && window.alert('Invalid index.');
+		}
+	};
+
+	$: $roverImageTotal && validateIndex();
+</script>
+
+<PageLayout />
