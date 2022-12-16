d4a5daf578645405949f2fcc773f6ba105dbc9e5
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:39:47 -0700

    create loading splash component

---
+++ src/components/LoadingSplash.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Robot } from '$comp';
+</script>
+
+<div class="loading space-y-4">
+	<Robot style="mx-auto" />
+
+	<p class="uppercase font-bold text-center text-3xl">
+		Loading images from <span class="text-secondary">Mars...</span>
+	</p>
+</div>
+
+<style>
+	.loading {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		-moz-transform: translateX(-50%) translateY(-50%);
+		-webkit-transform: translateX(-50%) translateY(-50%);
+		transform: translateX(-50%) translateY(-50%);
+	}
+</style>
