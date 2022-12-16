b4aee1adf85f2f3d28a6a2ba8f1fbb1a520bdf3e
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:39:23 -0700

    create header and footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { roverImageTotal } from '$lib/store';
+
+	const links = [
+		{ title: 'NASA', url: 'https://www.nasa.gov/perseverance' },
+		{ title: 'Wiki', url: 'https://wikiless.org/wiki/Perseverance_(rover)' },
+		{ title: 'Twitter', url: 'https://twitter.com/nasapersevere' },
+		{ title: 'Code', url: 'https://github.com/secondl1ght/rover-images' }
+	];
+</script>
+
+<footer
+	class="text-center md:text-left w-full md:h-40 border-t-8 border-black md:flex justify-between items-end p-4"
+>
+	<div class="uppercase font-bold text-sm">
+		<p>Launch date: <span class="text-secondary">2020-07-30</span></p>
+		<p>Landing date: <span class="text-secondary">2021-02-18</span></p>
+		<p>
+			Total images: <span class="text-secondary"
+				>{$roverImageTotal ? $roverImageTotal : 'calculating...'}</span
+			>
+		</p>
+	</div>
+
+	<div class="space-x-4 mt-2 md:mt-0">
+		{#each links as link}
+			<a href={link.url} target="_blank" rel="noreferrer" class="hover:text-secondary font-bold"
+				>{link.title}</a
+			>
+		{/each}
+	</div>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { page } from '$app/stores';
+	import { Robot } from '$comp';
+
+	const nav = [
+		{ title: 'Slideshow', url: '/' },
+		{ title: 'Latest', url: '/latest' },
+		{ title: 'Random', url: '/random' }
+	];
+
+	const reloadPage = (path) => {
+		if ($page.url.pathname === path) {
+			location.reload();
+		}
+	};
+</script>
+
+<header
+	class="text-center md:text-left w-full pb-[112px] md:pb-0 md:h-40 border-b-8 border-black md:flex justify-between p-4"
+>
+	<div class="uppercase font-bold">
+		<h1 class="text-3xl md:text-5xl">Perseverance</h1>
+		<p class="text-secondary text-xl">Mars rover</p>
+		<p class="text-xs">
+			Front Hazard Avoidance Camera <span class="hidden md:inline"> - Left</span>
+		</p>
+	</div>
+
+	<Robot style="absolute top-[152px] md:top-16 left-[calc(50vw-64px)] md:left-[calc(50vw-104px)]" />
+
+	<nav class="space-x-4 mt-2 md:mt-0">
+		{#each nav as link}
+			<a
+				on:click={() => reloadPage(link.url)}
+				href={link.url}
+				class="hover:text-secondary font-bold md:text-lg {$page.url.pathname === link.url
+					? 'border-b-4 border-secondary pb-1'
+					: ''}">{link.title}</a
+			>
+		{/each}
+	</nav>
+</header>
