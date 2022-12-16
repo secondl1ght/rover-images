f69e4d3260fa0444535919cb2ff1f61723b84123
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:43:36 -0700

    create alias for component directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$comp: path.resolve('src/components/index.js')
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
