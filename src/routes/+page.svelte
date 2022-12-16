0827214a8c2f6371e62574c5233a12e19472ad83
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-16 14:42:04 -0700

    add home slideshow home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onDestroy } from 'svelte';
+	import { RoverAPI } from '$lib/wrapper/rover-api';
+	import { clearSlideshow, roverImageTotal } from '$lib/store';
+	import { PageLayout } from '$comp';
+
+	const rover = new RoverAPI();
+
+	$: $roverImageTotal && rover.iterate($roverImageTotal);
+
+	onDestroy(() => $clearSlideshow && $clearSlideshow());
+</script>
+
+<PageLayout />
