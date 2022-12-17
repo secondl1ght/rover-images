ccc05972975c8c019943d16348d0f9e8e4b6f7a7
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-17 09:33:05 -0700

    add height to robot avatar

--- src/components/Robot.svelte
+++ src/components/Robot.svelte
@@ -7,5 +7,5 @@
 	fetchpriority="high"
 	src="/images/robot-avatar.jpg"
 	alt="robot avatar"
-	class="z-[100] w-32 md:w-48 {style} rounded-full border-black border-8"
+	class="z-[100] w-32 h-32 md:w-48 md:h-48 {style} rounded-full border-black border-8"
 />
